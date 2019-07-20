cb64adc5ba1526f68fdb0a3d5dd1c9c82c62dd9d
mtbsteve <schiwo1@gmail.com>
2019-07-20 14:39:11 +0200

    remove unnecessary packages:wq

--- Nvidia_JTX2_JP42/Ubuntu/2_install_packages.sh
+++ Nvidia_JTX2_JP42/Ubuntu/2_install_packages.sh
@@ -13,13 +13,14 @@ set -x
 sudo apt-get purge -y whoopsie
 
 # install a couple of packages from the local repo:
-sudo apt-get install -y --allow-unauthenticated libcudnn7-dev libcudnn7
+# JP42 comes with libcudnn7 installed so we dont need this
+# sudo apt-get install -y --allow-unauthenticated libcudnn7-dev libcudnn7
 
 # install packages common to all
 pushd ../../Common/Ubuntu
  ./install_packages.sh
 popd
  
-# live video related packages
+# live video related packages - not needed since gstreamer and livav 1.14 are installed with Jetpack 42
 # as of 201701121657, this fails to install gstreamer1.0-vaapi; remove || true when fixed.
-apt-get -y install gstreamer1.0 gstreamer1.0-libav || true
+# apt-get -y install gstreamer1.0 gstreamer1.0-libav || true
