12883b5587c85c020394f27138c1b664657b38cb
mtbsteve <schiwo1@gmail.com>
2019-07-21 12:12:55 +0200

    fixed path to apweb_start directory

--- Common/Ubuntu/apweb/autostart_apweb.sh
+++ Common/Ubuntu/apweb/autostart_apweb.sh
@@ -4,7 +4,9 @@
 set -e
 set -x
 
-pushd $HOME/start_apweb
+# pushd $HOME/start_apweb
+# we need to include the full path since this script is executed under root from rc.local
+pushd /home/apsync/start_apweb
 screen -L -d -m -S apweb -s /bin/bash ./start_apweb.sh >start_apweb.log 2>&1
 
 exit 0
