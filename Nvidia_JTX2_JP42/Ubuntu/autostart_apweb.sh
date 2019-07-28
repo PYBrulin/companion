a148504a5ba1c7992511052ab664ba06a8fecf77
mtbsteve <schiwo1@gmail.com>
2019-07-28 18:12:24 +0200

    fixes to support APStreamline

--- Nvidia_JTX2_JP42/Ubuntu/autostart_apweb.sh
+++ Nvidia_JTX2_JP42/Ubuntu/autostart_apweb.sh
@@ -6,7 +6,10 @@ set -x
 
 # pushd $HOME/start_apweb
 # we need to include the full path since this script is executed under root from rc.local
-pushd /home/apsync/start_apweb
-screen -L -d -m -S apweb -s /bin/bash ./start_apweb.sh >start_apweb.log 2>&1
+cd /home/apsync/start_apweb
+#fixed the logging 
+./start_apweb.sh > ./start_apweb.log &
+
+#:screen -L -d -m -S apweb -s /bin/bash ./start_apweb.sh >start_apweb.log 
 
 exit 0
