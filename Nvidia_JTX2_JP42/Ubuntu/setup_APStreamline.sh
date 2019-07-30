061451b04a5d580faa981c4077afbdaed1cae5a9
pybrulin <p.brulin@hexadrone.fr>
2019-07-30 11:59:13 +0200

    Update on GitHub syntax

--- Nvidia_JTX2_JP42/Ubuntu/setup_APStreamline.sh
+++ Nvidia_JTX2_JP42/Ubuntu/setup_APStreamline.sh
@@ -23,7 +23,7 @@ set -x
 # we need to install a modified version of APStreamline
 # includes defect fixes and the necessary adoptions for the TX2
 # please also follow the wiki for APStreamline on github
-pushd ~/Github
+pushd ~/GitHub
 rm -rf adaptive-streaming
 git clone -b video_streaming https://github.com/mtbsteve/APWeb.git
 pushd adaptive-streaming
